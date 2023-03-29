30f1be9c446d064b076dddfdc5951cf85469703a
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-29 16:34:29 -0700

    Add and link Game component (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 
+import Game from './components/Game/Game';
 import './style.css';
 
 const App = () => {
   return (
     <>
       <h1 className='text-4xl text-white bg-black'>Memory Game</h1>
+      <Game />
     </>
   );
 };
