ff3e606d9b8936a4a54941bfc95ad4166df4f249
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-28 20:56:04 -0700

    Add source map to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { argv } = require('process');
 
 const config = {
   entry: ['./src/index.tsx'],
@@ -39,6 +40,7 @@ const config = {
       },
     ],
   },
+  devtool: argv.mode === 'development' ? 'inline-source-map' : 'source-map',
   devServer: {
     static: {
       directory: './dist',
