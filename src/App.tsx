cfcf391e9d753436081a284622615d3802783e72
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-29 16:44:48 -0700

    Add state for choices

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Game from './components/Game/Game';
+import CardInfo from './interfaces/CardInfo';
 import './style.css';
 
 const App = () => {
+  const [choices, setChoices] = useState<CardInfo[]>([
+    {
+      imgUrl: 'bg-orange-50',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-100',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-200',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-300',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-400',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-500',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-600',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-700',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-800',
+      selected: false,
+    },
+    {
+      imgUrl: 'bg-orange-900',
+      selected: false,
+    },
+  ]);
+
   return (
     <>
       <h1 className='text-4xl text-white bg-black'>Memory Game</h1>
