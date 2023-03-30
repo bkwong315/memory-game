810b0af5bd3b1b491e79985e1217c3a0eddf7154
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-30 02:53:22 -0700

    Add high score tracker

--- src/App.tsx
+++ src/App.tsx
@@ -50,10 +50,12 @@ const App = () => {
   ];
   const [choices, setChoices] = useState<CardInfo[]>(defaultState);
   const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
   let shuffledChoices: CardInfo[] = shuffle(choices);
 
   const handleCardClick = (idx: number) => {
     if (choices[idx].selected) {
+      if (currentScore > highScore) setHighScore(currentScore);
       console.log('Selected choice already');
       setChoices(defaultState);
       setCurrentScore(0);
@@ -71,7 +73,8 @@ const App = () => {
     <>
       <h1 className='text-4xl text-white bg-black'>Memory Game</h1>
       <div className='score-container'>
-        <div className='current-score'>{currentScore}</div>
+        <div className='current-score'>Current Score: {currentScore}</div>
+        <div className='high-score'>High Score: {highScore}</div>
       </div>
       <Game choices={shuffledChoices} handleCardClick={handleCardClick} />
     </>
