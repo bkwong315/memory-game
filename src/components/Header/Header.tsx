78ffbf6732e85099edd24df54a7b51abfdef39ed
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-30 21:10:55 -0700

    Add styling to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,8 +9,8 @@ const Header = (props: HeaderProps) => {
   const { currentScore, highScore } = props;
 
   return (
-    <header>
-      <h1 className='text-4xl text-white bg-black'>Memory Game</h1>
+    <header className='flex bg-gradient-to-t from-red-700 to-red-500 shadow-md justify-between items-center bg-[#f50303] text-white p-4 font-extrabold'>
+      <h1 className='text-4xl'>Memory Game</h1>
       <div className='score-container'>
         <div className='current-score'>Current Score: {currentScore}</div>
         <div className='high-score'>High Score: {highScore}</div>
