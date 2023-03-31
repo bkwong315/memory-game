8bd3a7756e81bd4422906bfd18fab4b3867fe00b
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-30 21:11:49 -0700

    Add styling to Card component

--- src/components/Game/Card.tsx
+++ src/components/Game/Card.tsx
@@ -12,8 +12,10 @@ const Card = (props: CardProps) => {
   const { idx, onClickHandler } = props;
 
   return (
-    <div onClick={onClickHandler.bind(null, idx)}>
-      <img src={imgUrl} alt={imgUrl} />
+    <div
+      className='flex justify-center items-center transition hover:scale-105 hover:shadow-lg hover:shadow-red-600  bg-orange-300 border-solid border-4 border-[#fff235] rounded-lg p-4'
+      onClick={onClickHandler.bind(null, idx)}>
+      <img src={imgUrl} alt={imgUrl} className='h-full object-contain' />
     </div>
   );
 };
