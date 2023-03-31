b114adaab0daec9edead337425d801f9925ff3da
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-30 21:26:29 -0700

    Update header styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,10 +10,12 @@ const Header = (props: HeaderProps) => {
 
   return (
     <header className='flex bg-gradient-to-t from-red-700 to-red-500 shadow-md justify-between items-center bg-[#f50303] text-white p-4 font-extrabold'>
-      <h1 className='text-4xl'>Memory Game</h1>
-      <div className=''>
-        <div className='current-score'>Current Score: {currentScore}</div>
-        <div className='high-score'>High Score: {highScore}</div>
+      <h1 className='text-4xl shadow-inner shadow-slate-700 rounded p-4 py-2 bg-slate-800'>
+        Memory Game
+      </h1>
+      <div className='shadow-inner shadow-slate-700 rounded px-4 py-2 bg-slate-800'>
+        <div className='text-end'>Current Score: {currentScore}</div>
+        <div className='text-end'>High Score: {highScore}</div>
       </div>
     </header>
   );
